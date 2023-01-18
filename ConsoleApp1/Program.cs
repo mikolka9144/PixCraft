4c29e2d6f0a9d13b9589128e64dfec7c3526fee0
Mikołaj Chajewski <mikolajchajewski@gmail.com>
2023-01-18 23:35:45 +0100

    fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,6 @@ namespace ConsoleApp1
         private static void Main(string[] args)
         {
             Console.WriteLine(Convert.ToBase64String(File.ReadAllBytes("PixBlocks Compatiblity layer.dll")));
-            Console.WriteLine(Convert.ToBase64String(File.ReadAllBytes("PixCraft.dll")));
-            Console.WriteLine(Convert.ToBase64String(File.ReadAllBytes("Integration.dll")));
             Console.ReadKey();
         }
     }
